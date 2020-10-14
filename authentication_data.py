da4b2532cc28d3344a0870f4f0a21dbb24702a87
Andrew Quinn <53230903+AndrewQuinn2020@users.noreply.github.com>
2020-10-14 13:50:31 -0400

    removed delays bc they're big dumb actually

--- authentication_data.py
+++ authentication_data.py
@@ -13,51 +13,54 @@ access_token_secret = ""
 auth_data = [consumer_key, consumer_secret, access_token, access_token_secret]
 
 
-def beat(t=1):
-    sleep(t)
-    print("")
-    sleep(t)
-    return None
-
 if __name__ == "__main__":
+    print("-" * 80)
+
     print("Here the data you are currently authenticating with.")
     print("If these are empty, you will need to create a Twitter")
     print("application and get these.")
+    print("If you're having trouble finding them, try")
+    print("")
+    print("    https://developer.twitter.com/en/apps/")
+    print("")
+    print("and see if anything comes up for you. You might need to apply")
+    print("for a Twitter Developer account and make an app there if you're")
+    print("new!")
+    print("")
+    print("(Note this was written October 14, 2020)")
 
-    beat()
+    print("")
 
-    print("Consumer Key :: {}".format(consumer_key))
-    print("Consumer Secret :: {}".format(consumer_secret))
-    print("Access Token :: {}".format(access_token))
+    print("Consumer Key        :: {}".format(consumer_key))
+    print("Consumer Secret     :: {}".format(consumer_secret))
+    print("Access Token        :: {}".format(access_token))
     print("Access Token Secret :: {}".format(access_token_secret))
 
-    beat()
+    print("")
 
 
     print("**Letting this data be seen publicly is a security risk.**")
 
-    beat()
+    print("")
 
     print("If you're worried about forking this and accidentally pushing")
     print("your private data, I strongly recommend you navigate to this")
     print("directory and run the commend,")
 
-    beat()
+    print("")
 
     print("    git update-index --assume-unchanged authentication_data.py")
 
-    beat()
+    print("")
 
     print("This will make it so git stops tracking this file on your PC.")
 
-    beat()
+    print("")
 
     print("    git update-index --no-assume-unchanged authentication_data.py")
 
-    beat()
+    print("")
 
     print("will reverse this process.")
-
-    beat()
 
     print("-" * 80)

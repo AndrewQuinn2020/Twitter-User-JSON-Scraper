1d8c59080ec3991da759f0e91c58df54e231fe01
Andrew Quinn <53230903+AndrewQuinn2020@users.noreply.github.com>
2020-10-14 12:28:16 -0400

    Added `logging` and `colorlog` from my tutorial.

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/python3
 
+import logging     # installed by default
+import colorlog    # run python -m pip install colorlog
+
+# Setup
+
+logger = logging.getLogger()
+logger.setLevel(colorlog.colorlog.logging.DEBUG)
+
+handler = colorlog.StreamHandler()
+handler.setFormatter(colorlog.ColoredFormatter())
+logger.addHandler(handler)
+
+
 if __name__ == "__main__":
-    print("Hello, TenTweetsFrom.")
+    logger.debug("Hello, TenTweetsFrom.")
